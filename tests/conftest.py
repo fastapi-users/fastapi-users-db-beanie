bd17247c36ad17b2626f561247d08001b59e52e8
Vittorio Zamboni <vittorio.zamboni@gmail.com>
2022-06-24 20:55:46 +0100

    Replace get_event_loop with single methods in conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,16 @@ import pytest
 
 @pytest.fixture(scope="session")
 def event_loop():
-    """Force the pytest-asyncio loop to be the main one."""
-    loop = asyncio.get_event_loop()
+    """
+    Force the pytest-asyncio loop to be the main one.
+    If there is no running event loop, create one and
+    set as the current one.
+    """
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
     yield loop
 
 
